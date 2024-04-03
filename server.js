0e603f3b07ecf9fa7be21d29d238117c0fedbb66
thienanngo11122003 <132942173+thienanngo11122003@users.noreply.github.com>
2024-04-02 23:33:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,79 +11,96 @@
 
 
 const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const MoviesDB = require("./modules/moviesDB.js");
-const db = new MoviesDB();
-
 const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const userService = require(".modules/moviesDB.js");
+
 const HTTP_PORT = process.env.PORT || 8080;
 
-//middleware
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-//routes
-app.get('/', (req, res) => {
-    res.json({message: "API Listening"});
+app.post("/api/user/register", (req, res) => {
+    userService.registerUser(req.body)
+    .then((msg) => {
+        res.json({ "message": msg });
+    }).catch((msg) => {
+        res.status(422).json({ "message": msg });
+    });
 });
 
-db.initialize(process.env.MONGODB_CONN_STRING).then(()=>{
-    app.listen(HTTP_PORT, ()=>{
-        console.log(`server listening on: ${HTTP_PORT}`);
+app.post("/api/user/login", (req, res) => {
+    userService.checkUser(req.body)
+    .then((user) => {
+        res.json({ "message": "login successful"});
+    }).catch(msg => {
+        res.status(422).json({ "message": msg });
     });
-}).catch((err)=>{
-    console.log(err);
 });
 
-
-app.post("/api/movies", (req, res) => {
-    db.addNewMovie(req.body)
-      .then((movie) => {
-        res.status(201).json(movie);
-      })
-      .catch(() => {
-        console.error(err);
-        res.status(500).json({ err: 'Internal Server Error.' } ) 
-    });
-  });
-  
-  //AC
-  app.get("/api/movies", (req, res) => {
-    db.getAllMovies(req.query.page, req.query.perPage, req.query.title)
-      .then((movie) => {
-        res.status(201).json(movie);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).json({ err: 'Internal Server Error.' }) 
-      });
-  });
-
-app.get('/api/movies/:id', (req, res) => {
-    db.getMovieById(req.params.id)
-    .then((movie) => {
-      res.status(200).json(movie);
+app.get("/api/user/favourites", (req, res) => {
+    userService.getFavourites(req.user._id)
+    .then(data => {
+        res.json(data);
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
     })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
+
 });
 
-app.put('/api/movies/:id', (req, res) => {
-    db.updateMovieById(req.body, req.params.id)
-    .then((movie) => {
-      res.status(200).json(movie);
+app.put("/api/user/favourites/:id", (req, res) => {
+    userService.addFavourite(req.user._id, req.params.id)
+    .then(data => {
+        res.json(data)
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
     })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
 });
 
-app.delete('/api/movies/:id', (req, res) => {
-    db.deleteMovieById(req.params.id);
-    res.status(204).end();
+app.delete("/api/user/favourites/:id", (req, res) => {
+    userService.removeFavourite(req.user._id, req.params.id)
+    .then(data => {
+        res.json(data)
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+});
+
+app.get("/api/user/history", (req, res) => {
+    userService.getHistory(req.user._id)
+    .then(data => {
+        res.json(data);
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+
+});
+
+app.put("/api/user/history/:id", (req, res) => {
+    userService.addHistory(req.user._id, req.params.id)
+    .then(data => {
+        res.json(data)
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+});
+
+app.delete("/api/user/history/:id", (req, res) => {
+    userService.removeHistory(req.user._id, req.params.id)
+    .then(data => {
+        res.json(data)
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+});
+
+userService.connect()
+.then(() => {
+    app.listen(HTTP_PORT, () => { console.log("API listening on: " + HTTP_PORT) });
 })
-
-
-
+.catch((err) => {
+    console.log("unable to start the server: " + err);
+    process.exit();
+});
