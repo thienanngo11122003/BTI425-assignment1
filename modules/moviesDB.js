fcd936e0bd4c769d4f31333282edf863a4d286cc
thienanngo11122003 <132942173+thienanngo11122003@users.noreply.github.com>
2024-04-02 23:33:28 -0400

    Update moviesDB.js

--- modules/moviesDB.js
+++ modules/moviesDB.js
@@ -1,99 +1,177 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const movieSchema = new Schema({
-  plot: String,
-  genres: [String],
-  runtime: Number,
-  cast: [String],
-  num_mflix_comments: Number,
-  poster: String,
-  title: String,
-  fullplot: String,
-  languages: [String],
-  released: Date,
-  directors: [String],
-  rated: String,
-  awards: {
-    wins: Number,
-    nominations: Number,
-    text: String
-  },
-  lastupdated: Date,
-  year: Number,
-  imdb: {
-    rating: Number,
-    votes: Number,
-    id: Number
-  },
-  countries: [String],
-  type: String,
-  tomatoes: {
-    viewer: {
-      rating: Number,
-      numReviews: Number,
-      meter: Number
+let mongoDBConnectionString = process.env.MONGO_URL;
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true
     },
-    dvd: Date,
-    lastUpdated: Date
-  }
-}
-);
+    password: String,
+    favourites: [String],
+    history: [String]
+});
 
-module.exports = class MoviesDB {
-  constructor() {
-    // We don't have a `Movie` object until initialize() is complete
-    this.Movie = null;
-  }
+let User;
 
-  // Pass the connection string to `initialize()`
-  initialize(connectionString) {
-    return new Promise((resolve, reject) => {
-      const db = mongoose.createConnection(
-        connectionString,
-        {
-          useNewUrlParser: true,
-          useUnifiedTopology: true
-        }
-      );
+module.exports.connect = function () {
+    return new Promise(function (resolve, reject) {
+        let db = mongoose.createConnection("mongodb+srv://thienanngo11122003:thienandeptrai123@cluster0.rwtzgkw.mongodb.net/thienanngo?retryWrites=true&w=majority&appName=Cluster0");
 
-      db.once('error', (err) => {
-        reject(err);
-      });
-      db.once('open', () => {
-        this.Movie = db.model("movies", movieSchema);
-        resolve();
-      });
+        db.on('error', err => {
+            reject(err);
+        });
+
+        db.once('open', () => {
+            User = db.model("users", userSchema);
+            resolve();
+        });
     });
-  }
+};
 
-  async addNewMovie(data) {
-    const newMovie = new this.Movie(data);
-    await newMovie.save();
-    return newMovie;
-  }
+module.exports.registerUser = function (userData) {
+    return new Promise(function (resolve, reject) {
 
-  getAllMovies(page, perPage, title) {
-    let findBy = title ? { title } : {};
+        if (userData.password != userData.password2) {
+            reject("Passwords do not match");
+        } else {
 
-    if (+page && +perPage) {
-      return this.Movie.find(findBy).sort({ year: +1 }).skip((page - 1) * +perPage).limit(+perPage).exec();
-    }
+            bcrypt.hash(userData.password, 10).then(hash => {
 
-    return Promise.reject(new Error('page and perPage query parameters must be valid numbers'));
-  }
+                userData.password = hash;
 
-  getMovieById(id) {
-    return this.Movie.findOne({ _id: id }).exec();
-  }
+                let newUser = new User(userData);
 
-  updateMovieById(data, id) {
-    return this.Movie.updateOne({ _id: id }, { $set: data }).exec();
-  }
+                newUser.save().then(() => {
+                    resolve("User " + userData.userName + " successfully registered");  
+                }).catch(err => {
+                    if (err.code == 11000) {
+                        reject("User Name already taken");
+                    } else {
+                        reject("There was an error creating the user: " + err);
+                    }
+                })
+            }).catch(err => reject(err));
+        }
+    });
+};
 
-  deleteMovieById(id) {
-    return this.Movie.deleteOne({ _id: id }).exec();
-  }
+module.exports.checkUser = function (userData) {
+    return new Promise(function (resolve, reject) {
+
+        User.findOne({ userName: userData.userName })
+            .exec()
+            .then(user => {
+                bcrypt.compare(userData.password, user.password).then(res => {
+                    if (res === true) {
+                        resolve(user);
+                    } else {
+                        reject("Incorrect password for user " + userData.userName);
+                    }
+                });
+            }).catch(err => {
+                reject("Unable to find user " + userData.userName);
+            });
+    });
+};
+
+module.exports.getFavourites = function (id) {
+    return new Promise(function (resolve, reject) {
+
+        User.findById(id)
+            .exec()
+            .then(user => {
+                resolve(user.favourites)
+            }).catch(err => {
+                reject(`Unable to get favourites for user with id: ${id}`);
+            });
+    });
 }
 
-//module.exports = {}
+module.exports.addFavourite = function (id, favId) {
+
+    return new Promise(function (resolve, reject) {
+
+        User.findById(id).exec().then(user => {
+            if (user.favourites.length < 50) {
+                User.findByIdAndUpdate(id,
+                    { $addToSet: { favourites: favId } },
+                    { new: true }
+                ).exec()
+                    .then(user => { resolve(user.favourites); })
+                    .catch(err => { reject(`Unable to update favourites for user with id: ${id}`); })
+            } else {
+                reject(`Unable to update favourites for user with id: ${id}`);
+            }
+
+        })
+
+    });
+
+
+}
+
+module.exports.removeFavourite = function (id, favId) {
+    return new Promise(function (resolve, reject) {
+        User.findByIdAndUpdate(id,
+            { $pull: { favourites: favId } },
+            { new: true }
+        ).exec()
+            .then(user => {
+                resolve(user.favourites);
+            })
+            .catch(err => {
+                reject(`Unable to update favourites for user with id: ${id}`);
+            })
+    });
+}
+
+module.exports.getHistory = function (id) {
+    return new Promise(function (resolve, reject) {
+
+        User.findById(id)
+            .exec()
+            .then(user => {
+                resolve(user.history)
+            }).catch(err => {
+                reject(`Unable to get history for user with id: ${id}`);
+            });
+    });
+}
+
+module.exports.addHistory = function (id, historyId) {
+
+    return new Promise(function (resolve, reject) {
+
+        User.findById(id).exec().then(user => {
+            if (user.favourites.length < 50) {
+                User.findByIdAndUpdate(id,
+                    { $addToSet: { history: historyId } },
+                    { new: true }
+                ).exec()
+                    .then(user => { resolve(user.history); })
+                    .catch(err => { reject(`Unable to update history for user with id: ${id}`); })
+            } else {
+                reject(`Unable to update history for user with id: ${id}`);
+            }
+        })
+    });
+}
+
+module.exports.removeHistory = function (id, historyId) {
+    return new Promise(function (resolve, reject) {
+        User.findByIdAndUpdate(id,
+            { $pull: { history: historyId } },
+            { new: true }
+        ).exec()
+            .then(user => {
+                resolve(user.history);
+            })
+            .catch(err => {
+                reject(`Unable to update history for user with id: ${id}`);
+            })
+    });
+}
